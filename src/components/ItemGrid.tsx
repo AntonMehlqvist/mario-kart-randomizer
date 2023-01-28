bb9e77021e3766d73f4df0e79e3e0faa1f8d3081
Anton Mehlqvist <antonmehlqvist@outlook.com>
2023-01-28 13:28:17 +0100

    fixed bullet bill bug

--- src/components/ItemGrid.tsx
+++ src/components/ItemGrid.tsx
@@ -23,7 +23,7 @@ export const ItemGrid: Component<Props> = props => {
 								<img
 									src={item().imgSrc}
 									alt={item.name}
-									class="absolute inset-0 left-1/2 max-h-full -translate-x-1/2 object-contain"
+									class="absolute inset-0 left-1/2 top-1/2 max-h-full -translate-x-1/2 -translate-y-1/2 object-contain"
 								/>
 							</div>
 						</div>
