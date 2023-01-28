5402797b950816ef3e3d5208b4e509d88e7440e3
Anton Mehlqvist <antonmehlqvist@outlook.com>
2023-01-28 12:58:05 +0100

    added extension recommendations and prettier

--- src/components/ItemGrid.tsx
+++ src/components/ItemGrid.tsx
@@ -1,36 +1,35 @@
-import { Component, createEffect, For, Index } from "solid-js";
-import type { GameItem } from "../gameInfo";
+import { Component, createEffect, For, Index } from 'solid-js';
+import type { GameItem } from '../gameInfo';
 type Props = {
-  items: GameItem[];
-  activeItems: string[];
+	items: GameItem[];
+	activeItems: string[];
 };
 
 type ItemGridItem = GameItem & { isActive: boolean };
 
-export const ItemGrid: Component<Props> = (props) => {
-  const items = () =>
-    props.items.map((item) => ({
-      ...item,
-      isActive: props.activeItems.includes(item.name),
-    }));
-  return (
-    <div class="grid grid-cols-6 max-w-6xl mx-auto gap-3">
-      <Index each={items()}>
-        {(item) => {
-          return (
-            <div
-              class={`aspect-square w-full p-[15%] bg-slate-100 transition-opacity rounded-md
-						${!item().isActive ? "opacity-50" : ""}`}
-            >
-              <img
-                src={item().imgSrc}
-                alt={item.name}
-                class="aspect-square object-contain mix-blend-multiply"
-              />
-            </div>
-          );
-        }}
-      </Index>
-    </div>
-  );
+export const ItemGrid: Component<Props> = props => {
+	const items = () =>
+		props.items.map(item => ({
+			...item,
+			isActive: props.activeItems.includes(item.name),
+		}));
+	return (
+		<div class="mx-auto grid max-w-6xl grid-cols-6 gap-3">
+			<Index each={items()}>
+				{item => {
+					return (
+						<div
+							class={`aspect-square w-full rounded-md bg-slate-100 p-[15%] transition-opacity
+						${!item().isActive ? 'opacity-50' : ''}`}>
+							<img
+								src={item().imgSrc}
+								alt={item.name}
+								class="aspect-square object-contain mix-blend-multiply"
+							/>
+						</div>
+					);
+				}}
+			</Index>
+		</div>
+	);
 };
