545e1140721d1f4bfa17edb626a850039ab2defe
Anton Mehlqvist <antonmehlqvist@outlook.com>
2023-01-29 12:56:36 +0100

    WIP

--- src/components/RandomBox.tsx
+++ src/components/RandomBox.tsx
@@ -8,37 +8,72 @@ type Props = {
 
 export default function RandomBox(props: Props) {
 	let boxContent: HTMLDivElement | undefined;
+	let boxTitle: HTMLDivElement | undefined;
 	const [boxContentHeight, setBoxContentHeight] = createSignal(0);
+	const [boxTitleHeight, setBoxTitleHeight] = createSignal(0);
 
 	onMount(() => {
-		if (!boxContent || !boxContent.children) return;
+		if (
+			!boxContent ||
+			!boxContent.children ||
+			!boxTitle ||
+			!boxTitle.children
+		) {
+			return;
+		}
+
 		setBoxContentHeight(
 			boxContent.offsetHeight - boxContent.children[0].clientHeight
+		);
+
+		setBoxTitleHeight(
+			boxTitle.offsetHeight - boxTitle.children[0].clientHeight
 		);
 	});
 
 	return (
-		<div class="h-32 w-28 overflow-hidden rounded-md bg-slate-100 bg-opacity-75">
-			<div
-				style={{
-					transform: props.isSpinning
-						? `translateY(-${boxContentHeight()}px)`
-						: 'translateY(0)',
-				}}
-				class="transition-transform duration-[2500ms]"
-				ref={boxContent}>
-				<For each={props.gameItems}>
-					{item => (
-						<div class="flex h-32 items-center justify-center p-3">
-							<img
-								height={128}
-								width={77}
-								src={item.imgSrc}
-								alt={item.name}
-							/>
-						</div>
-					)}
-				</For>
+		<div>
+			<div class="relative h-36 w-28 overflow-hidden rounded-md bg-slate-100 bg-opacity-75">
+				<div
+					style={{
+						transform: props.isSpinning
+							? `translateY(-${boxContentHeight()}px)`
+							: 'translateY(0)',
+					}}
+					class="transition-transform duration-[2500ms]"
+					ref={boxContent}>
+					<For each={props.gameItems}>
+						{item => (
+							<div class="flex h-32 items-center justify-center p-3">
+								<img
+									height={128}
+									width={77}
+									src={item.imgSrc}
+									alt={item.name}
+								/>
+							</div>
+						)}
+					</For>
+				</div>
+
+				<div class="absolute bottom-0 left-0 mt-2 h-8 w-28 overflow-hidden rounded-md  bg-slate-200">
+					<div
+						style={{
+							transform: props.isSpinning
+								? `translateY(-${boxTitleHeight()}px)`
+								: 'translateY(0)',
+						}}
+						class="transition-transform duration-[2500ms]"
+						ref={boxTitle}>
+						<For each={props.gameItems}>
+							{item => (
+								<div class="flex h-8 items-center justify-center">
+									<p>{item.name.substring(0, 8)}...</p>
+								</div>
+							)}
+						</For>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
