eb85fbd350b8df9e819911fb1df9f4d883dfc01b
Anton Mehlqvist <antonmehlqvist@outlook.com>
2023-02-12 16:47:29 +0100

    extended item randomizer

--- src/utils/getRandomItemSelection.ts
+++ src/utils/getRandomItemSelection.ts
@@ -1,6 +1,28 @@
 import random from 'random';
+import { items } from '../gameInfo';
 import { getItemNames } from './getItemNames';
+import { shuffleArray } from './shuffleArray';
 
 export const getRandomItemSelection = () => {
-	return getItemNames().filter(() => random.boolean());
+	let numberOfItems = random.int(1, items.length);
+	const numberOfShuffles = random.int(1, 20);
+
+	if (numberOfItems > 14) {
+		numberOfItems =
+			random.int(0, 100) < 75
+				? numberOfItems - random.int(1, 5)
+				: numberOfItems;
+	} else if (numberOfItems < 8) {
+		numberOfItems =
+			random.int(0, 100) < 75
+				? numberOfItems + random.int(1, 5)
+				: numberOfItems;
+	}
+
+	let gameItemNames = getItemNames();
+	for (let i = 0; i < numberOfShuffles; i++) {
+		gameItemNames = shuffleArray(gameItemNames);
+	}
+
+	return gameItemNames.slice(0, numberOfItems);
 };
